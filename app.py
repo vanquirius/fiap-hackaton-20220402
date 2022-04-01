dcc4eaea1ff7474a00d2ca00d30b113a79378516
Marcelo Góes <marcelogoes@gmail.com>
2022-04-01 18:18:08 -0300

    primeiro commit app teste

---
+++ app.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+# Hackaton FIAP
+# Enilson dos Santos Jr., Klaus Araújo, Lilian Barbosa, Marcelo Góes,
+# Müller Silva, Nicolas Osmundo, Roberto Ribeiro, Vinicius Montouro
+# 2022-04-02
+
+from flask import Flask
+from flask_wtf.csrf import CSRFProtect
+import os
+
+app = Flask(__name__)
+csrf = CSRFProtect(app)
+csrf.init_app(app)
+
+@app.route("/")
+def pagina_inicial():
+    return "Hello World - Marcelo Ambrosio de Góes - rm341073"
+
+if __name__ == '__main__':
+#    app.run()
+    port = os.getenv('PORT')
+    app.run('0.0.0.0', port=port)
---
+++ test.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+# Hackaton FIAP
+# Enilson dos Santos Jr., Klaus Araújo, Lilian Barbosa, Marcelo Góes,
+# Müller Silva, Nicolas Osmundo, Roberto Ribeiro, Vinicius Montouro
+# 2022-04-02
+
+from app import app
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        # cria uma instância do unittest, precisa do nome "setUp"
+        self.app = app.test_client()
+
+        # envia uma requisicao GET para a URL
+        self.result = self.app.get('/')
+
+    def test_requisicao(self):
+        # compara o status da requisicao (precisa ser igual a 200)
+        self.assertEqual(self.result.status_code, 200)
+
+    def test_conteudo(self):
+        # verifica o retorno do conteudo da pagina
+        self.assertEqual(self.result.data.decode('utf-8'), "Hello World")
